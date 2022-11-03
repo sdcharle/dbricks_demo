339e8e68314d46f146ffcf6eb1f2f5649e3a18b5
Seve <mistasteve@gmail.com>
2022-11-03 19:56:15 +0000

    modelin

--- task.R
+++ task.R
@@ -1,6 +1,26 @@
-# dumb modeling task :)
+# a goofy modeling task :)
 
 library(dplyr)
 library(xgboost)
 library(readr)
 
+# IRIS sure is lame
+
+data(iris)
+
+iris_X = iris[, 1:4]
+iris_y = as.integer(iris[, 5]) - 1
+
+# Generate R model matrix
+iris.matrix = model.matrix(~ . - 1, data = iris_X)
+
+# Generate XGBoost DMatrix and feature map based on R model matrix
+iris.DMatrix = xgb.DMatrix(iris.matrix, label = iris_y)
+iris.fmap = as.fmap(iris.matrix)
+
+# Train a model
+iris.xgb = xgboost(data = iris.DMatrix, 
+                   missing = NULL, 
+                   objective = "multi:softmax", 
+                   num_class = 3, 
+                   nrounds = 200)
