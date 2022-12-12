5be17154f4e886f342f187fdd6e73e658bbc551f
sdcharle <sdcharle>
2022-12-12 03:11:55 +0000

    tried in new repo

--- Notebooks/renv_use.r
+++ Notebooks/renv_use.r
@@ -1,26 +1,18 @@
 # Databricks notebook source
-library(A3)
+library(xgboost)
 # should flop in new setting
 
 # COMMAND ----------
 
-# if rerun on your cluster will get error.
+# if rerun multiple times within a repo, will get an error.
 # additionally if you are using a repo - that is persisted, thus you do not need to init.
 
 #renv::init(settings = list(external.libraries=.libPaths()))
 
 .libPaths(c(.libPaths()[2], .libPaths()))
-renv::restore(lockfile="/dbfs/SDC_Tests/renv.lock", exclude=c("Rserve", "SparkR"))
+renv::restore(exclude=c("Rserve", "SparkR"))
 
 # COMMAND ----------
 
-library(A3)
+library(xgboost)
 # should be good.
-
-# COMMAND ----------
-
-# btw what does that file look like
-# actually just use regular snapshot if you are using a repo!
-# force avoids 'Yes' step
-renv::snapshot(force=TRUE)
-
